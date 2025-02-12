6d8db20e115c1d47d5f278041db6823d50c81df9
dewarrob <dewarrob@msu.edu>
2025-02-11 21:56:15 -0500

    fixed loading msg

--- frontend/src/features/auth/Login.js
+++ frontend/src/features/auth/Login.js
@@ -55,7 +55,11 @@ const Login = () => {
 
     const handlePwdInput = (e) => setPwd(e.target.value)
 
-    const content = isLoading ? <h1>Loading...</h1> : (
+    const content = isLoading ? <h1>This page may take up to 50 seconds to load because I am using the free plan of my hosting service. Welcome to the site! My name is Robby Dewar, and I am primarily using this site to showcase my abilities as a web designer. Thank you for visiting! Please let me know if you encounter any bugs. You can contact me at dewarrob@msu.edu.
+
+    If you are viewing this site because I applied for a job at your company, I believe I would make a great addition to any team. I hold a bachelor's degree in Computer Science from Michigan State University, and most of my experience lies in developing web applications. I am a kind and open-minded individual who believes that computers have the potential to make the world an infinitely better place.
+    
+    I understand that many people fear a future dominated by technology, worrying that it may cause us to lose our connections to each other and to nature. I believe that computers can actually bring people closer to the world and to those around them. :)</h1> : (
         <section className="login">
             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
 
