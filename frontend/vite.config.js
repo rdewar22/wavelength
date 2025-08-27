ae3802ceaec86915e799375baba0a52249db6612
Robby Dewar <dewarrob@msu.edu>
2025-08-26 20:13:04 -0400

    changed vite.config.js to ensure proper handling of history API

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-  server: {
-    port: 3000,
-    hmr: {
-      overlay: true,
-    },
-  },
+  plugins: [react()],
+  base: "/",
   build: {
-    outDir: 'dist',
+    outDir: "dist"
+  },
+  server: {
+    historyApiFallback: true
   }
 });
